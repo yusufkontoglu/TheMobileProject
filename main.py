25377c84d39618aeb8aef705ad7bf594534d6c37
yusufkontoglu <yusufkontoglu@gmail.com>
2021-01-24 20:30:29 +0300

    24.01.2021

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import os
 
+os.system('cls')
 # screen size
+# os.system("mode con: cols=50 lines=31")
 
-os.system("mode con: cols=50 lines=30")
+print('\n\n1- 1. sayfa\n2- 2. sayfa\n3- 3. sayfa\n\n')
+val = int(input('Sayfa numarasını giriniz.   '))
+print('\n\n****************')
+
+if val == 1:
+    print("\n\n1.sayfadasınız\n\n\n\n****************")
+elif val == 2:
+    print("\n\n2.sayfadasınız\n\n\n\n****************")
+else:
+    print("\n\n2.sayfadasınız\n\n\n\n****************")
